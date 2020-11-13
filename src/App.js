3c43bd8eba007ee9f7ab2e58ec2cc0a9918a789b
Samuel Lee <samuelxlee@gmail.com>
2020-11-13 08:40:04 -0800

    created basic components for the challenge SPA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import {Route, Link, Switch} from "react-router-dom";
+import Home from "./Home";
+import Form from "./Form";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <nav>
+        <h1>Lambda Eats</h1>
+
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Order Now</Link>
+        </div>
+      </nav>
+
+      <Switch>
+      <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path={"/pizza"}>
+          <Form />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
